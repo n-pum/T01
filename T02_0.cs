9b9f5e0201dc7323509091df39d117eb490a4cec
N-pum <n-pum@hotmail.es>
2021-06-16 11:52:34 +0200

    Agregar archivos de proyecto.

---
+++ T02_0.cs
@@ -0,0 +1,14 @@
+﻿/*using System;
+
+namespace T02
+{
+    class T02_0
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
+*/
